4294fbabdbc64b04b3b9074fd1aca8791c4815a4
a1ex-af <school@rozaazora.by>
2018-11-28 21:53:49 +0300

    change content

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,13 @@
 module.exports = {
-    plugins: [`gatsby-plugin-netlify-cms`],
+    plugins: [
+        `gatsby-plugin-netlify-cms`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/site/content/`,
+                name: "markdown-pages",
+            },
+        },
+        `gatsby-transformer-remark`,
+    ],
 }
--- site/content/blog/khkb.md
+++
@@ -1,6 +0,0 @@
----
-path: ljljl
-date: 2018-11-28T17:40:35.745Z
-title: khkb
----
-llkjk
