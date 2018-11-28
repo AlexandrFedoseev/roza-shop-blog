f889f311799438bce539aaf890bb863e8091dcaf
a1ex-af <school@rozaazora.by>
2018-11-28 23:34:55 +0300

    adds styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,12 @@
 module.exports = {
     plugins: [
+        {
+            resolve: `gatsby-plugin-typography`,
+            options: {
+                pathToConfigModule: `src/utils/typography.js`,
+            },
+        },
+        `gatsby-plugin-sass`,
         `gatsby-plugin-netlify-cms`,
         {
             resolve: `gatsby-source-filesystem`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,55 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+import styles from "./index.module.scss"
+import Container from "../components/container"
+console.log(styles)
+
+const User = props => (
+    <div className={styles.user}>
+        <img src={props.avatar} className={styles.avatar} alt="" />
+        <div className={styles.description}>
+            <h2 className={styles.username}>{props.username}</h2>
+            <p className={styles.excerpt}>{props.excerpt}</p>
+        </div>
+    </div>
+)
+
+export default () => (
+    <Container>
+        <h1>Richard Hamming on Luck</h1>
+        <div>
+        <p>
+            From Richard Hamming’s classic and must-read talk, “
+            <a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
+            You and Your Research
+            </a>
+            ”.
+        </p>
+        <blockquote>
+            <p>
+            There is indeed an element of luck, and no, there isn’t. The prepared
+            mind sooner or later finds something important and does it. So yes, it
+            is luck.{" "}
+            <em>
+                The particular thing you do is luck, but that you do something is
+                not.
+            </em>
+            </p>
+        </blockquote>
+        </div>
+        <p>Posted April 09, 2011</p>
+        <h1>About CSS Modules</h1>
+        <p>CSS Modules are cool</p>
+        <User
+        username="Jane Doe"
+        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+        excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+        />
+
+        <User
+        username="Bob Smith"
+        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
+        excerpt="I'm Bob smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+        />
+    </Container>
+)
