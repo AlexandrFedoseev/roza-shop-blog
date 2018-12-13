1fe0688587776e07f9ca4ef4b4fa0e389f692bb9
Unknown <school@rozaazora.by>
2018-12-14 01:39:59 +0300

    changes cons sextion

--- src/components/consSection.js
+++ src/components/consSection.js
@@ -4,8 +4,8 @@ import consSectionStyles from "./consSection.module.scss"
 const Con = props => (
     <div className={consSectionStyles.consItem}>
         <img src={props.image} />
-        <span className={consSectionStyles.title}>
-            {props.title}<br/>{props.title2}
+        <span className={consSectionStyles.conText}>
+            {props.conText}
         </span>
     </div>
 )
@@ -13,9 +13,17 @@ const Con = props => (
 export default () => (
     <div>
         <div className={consSectionStyles.container} style={{marginBottom: '20px'}}>
-            <Con image={'/assets/images/bouquet.jpg'} title=" Букет" title2="на счастье"></Con>
-            <Con image={'/assets/images/professionals.jpg'} title={'мы - комманда'} title2="профессионалов"></Con>
-            <Con image={'/assets/images/world-flowers.jpg'} title={'удивительные цветы'} title2="из 10 стран мира">></Con>
+            <Con image={'/assets/images/double-tick.png'} conText={"Опыт - дарим положительные эмоции более 10 лет"}></Con>
+            <Con image={'/assets/images/double-tick.png'} conText={"Эксклюзивный ассортимент из 10 стран мира"}></Con>
+            <Con image={'/assets/images/double-tick.png'} conText={"Авторский подход к каждому букету - будь то цветочный комплимент или изысканный букет"}></Con>
+            <Con image={'/assets/images/double-tick.png'} conText={'Наш салон - это место, где в гармонии живут цветы, красота, уют и искусство. Приходите к нам в гости и окунитесь в прекрасный мир салона цветов "Роза Азора"'}></Con>
         </div>
     </div>
 )
+
+
+
+
+
+
+
