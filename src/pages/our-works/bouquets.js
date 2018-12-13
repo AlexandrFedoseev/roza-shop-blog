3e9bd9d6ece0dc7888bac16bbe1d8470310f09a1
Unknown <school@rozaazora.by>
2018-12-14 00:39:26 +0300

    changes button styles

--- src/pages/our-works/bouquets.js
+++ src/pages/our-works/bouquets.js
@@ -9,11 +9,13 @@ import ContactForm from "../../components/contactForm"
 export default () => (
     <main>
         <Header title={'Букеты'} image={"/assets/images/works/1.jpg"}></Header>
-        <Section title={'Как выглядят наши работы'}></Section>
+        <Section title={'Букет невесты'}></Section>
         <WorksGallery></WorksGallery>
-        <Section title={'Как выглядят наши работы'}></Section>
+        <Section title={'Подарочные букеты'}></Section>
         <WorksGallery></WorksGallery>
-        <Section title={'Как выглядят наши работы'}></Section>
+        <Section title={'Монобукеты'}></Section>
+        <WorksGallery></WorksGallery>
+        <Section title={'Мужские букеты'}></Section>
         <WorksGallery></WorksGallery>
         <Section title={'Остались вопросы?'}>
             <ContactForm></ContactForm>
